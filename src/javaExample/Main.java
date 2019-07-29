321921211d92134c1e952f8c4a8f961a69e7d8e0
Geferson Pillaca <gpillacag@gmail.com>
2019-07-28 20:35:14 -0500

    init

---
+++ src/javaExample/Main.java
@@ -0,0 +1,7 @@
+package javaExample;
+
+public class Main {
+    public static void main(String[]args){
+        System.out.println("Hello world Java");
+    }
+}
---
+++ src/kotlinExample/Main.kt
@@ -0,0 +1,5 @@
+package kotlinExample
+
+fun main(args: Array<String>) {
+    print("Hello world kotlin")
+}
